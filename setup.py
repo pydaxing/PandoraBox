fffc139867dbb2b2dbf5f8c1473de15aaf371092
chengsong.cs <chengsong.cs@alibaba-inc.com>
2024-06-21 23:34:28 +0800

    rename readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     description='Python Box Is All You Need. You Can Create Python Environment, Execut Python, Close Python Environment Freely and Easily.',
-    long_description=open('README.md').read(),
+    long_description=open('README.MD').read(),
     long_description_content_type='text/markdown',
     author='pydaxing',
     author_email='pydaxing@gmail.com',
